d7528e2a5f68a63374f4d092d0714d505a623cde
WSL0809 <wslzzy@outlook.com>
2022-04-26 03:21:35 +0800

    wsl0809

--- Sort/main/main.go
+++ Sort/main/main.go
@@ -1,10 +1,13 @@
 package main
-import "fmt"
-import "leetcode/Sort/utils"
+
+import (
+	"fmt"
+	"leetcode/Sort/utils"
+)
 
 func main() {
 
-    list2 := []int{5, 9, 1, 6, 8, 14, 6, 49, 25, 4, 6, 3, 9233, 2222, 4567}
-    utils.InsertSort(list2)
-    fmt.Println("直接插入排序", list2)
+	list2 := []int{5, 9, 1, 6, 8, 14, 6, 49, 25, 4, 6, 3, 9233, 2222, 4567}
+	utils.InsertSort(list2)
+	fmt.Println("直接插入排序111111", list2)
 }
